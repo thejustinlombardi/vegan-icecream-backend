cd0154630dac97cc001d58519526a5b766db80d6
thejustinlombardi <thejustinlombardi@gmail.com>
2022-02-09 11:33:45 -0500

    Add django framework and basic setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vegan_icecream_django.wsgi
---
+++ settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE vegan_icecream;
+CREATE USER vegan_icecreamuser WITH PASSWORD 'vegan_icecream';
+GRANT ALL PRIVILEGES ON DATABASE vegan_icecream TO vegan_icecreamuser;
